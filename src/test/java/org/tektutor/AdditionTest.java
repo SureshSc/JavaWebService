36b0cf5c078ed683793dfdd48d4e9d4282fb02a4
Suresh S C <scs.6190@gmail.com>
2019-07-02 14:00:42 +0530

    test case from BDD added

---
+++ src/test/java/org/tektutor/AdditionTest.java
@@ -0,0 +1,69 @@
+package org.tektutor;
+
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+import junit.framework.Assert;
+
+public class AdditionTest {
+	
+	@Given("Two numbers in JSON format string")
+	public void twoNumbersInJSONFormatString() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new cucumber.api.PendingException();
+	}
+
+	@When("i click add")
+	public void iClickAdd() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new cucumber.api.PendingException();
+	}
+
+	@Then("sum of two numbers should be shown")
+	public void sumOfTwoNumbersShouldBeShown() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new cucumber.api.PendingException();
+	}
+	
+//	public Calculator calc = new Calculator();
+//	
+//	@Given("Two numbers in JSON format string")
+//	public void twoNumbersInJSONFormatString() {
+//	    // Write code here that turns the phrase above into concrete actions
+//	    throw new cucumber.api.PendingException();
+//	}
+//
+//	@When("i click add")
+//	public void iClickAdd() {
+//	    // Write code here that turns the phrase above into concrete actions
+//	    throw new cucumber.api.PendingException();
+//	}
+//
+//	@Then("sum of two numbers should be shown")
+//	public void sumOfTwoNumbersShouldBeShown() {
+//	    // Write code here that turns the phrase above into concrete actions
+//		String result = calc.doAddition("{\"number_one\":10,\"number_two\":2}");
+//	    Assert.assertNotNull(result);
+//	}
+//
+//	@Given("i enter two numbers")
+//	public void iEnterTwoNumbers() {
+//	    // Write code here that turns the phrase above into concrete actions
+//		String result = calc.doAddition("{\"number_one\":10,\"number_two\":2}");
+//	    Assert.assertNotNull(result);
+//	}
+//
+//	@When("i add two numbers")
+//	public void iAddTwoNumbers() {
+//	    // Write code here that turns the phrase above into concrete actions
+//		String result = calc.doAddition("{\"number_one\":10,\"number_two\":2}");
+//	    Assert.assertNotNull(result);
+//	}
+//
+//	@Then("i should get sum of two number")
+//	public void iShouldGetSumOfTwoNumber() {
+//	    // Write code here that turns the phrase above into concrete actions
+//	    throw new cucumber.api.PendingException();
+//	}
+//	
+}
---
+++ src/test/java/org/tektutor/calculator.feature
@@ -0,0 +1,6 @@
+Feature: Calculator
+	Scenario: Addition function
+		Given Two numbers in JSON format string
+		When i click add
+		Then sum of two numbers should be shown 
+		
